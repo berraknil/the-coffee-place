f6db5680923d18ce7a2d5e02a8e09d3eb7dfb880
Berrak Nil <berraknil@gmail.com>
2017-04-29 13:13:10 +0300

    navigation styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import React, { Component } from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Menu from './components/Menu'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Navigation from './components/Navigation'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <header>
-          <ul className="navigation">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/menu">Menu</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
-        </header>
+        <Navigation />
         <main>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './Navigation.css'
+
+const Navigation = () => (
+  <nav>
+    <ul className="navigation">
+      <li><NavLink activeClassName="active" exact to="/">Home</NavLink></li>
+      <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+      <li><NavLink activeClassName="active" to="/menu">Menu</NavLink></li>
+      <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
