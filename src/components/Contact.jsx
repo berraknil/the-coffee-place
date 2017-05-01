f232460a4ccc05c995ccaa3a755a1890a75137ce
Berrak Nil <berraknil@gmail.com>
2017-05-01 18:04:23 +0300

    contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,10 @@ import { connect } from 'react-redux'
 import { fetchPage } from '../actions/index'
 /* import PropTypes from 'prop-types'*/
 import './Contact.css'
+import twitter from '../icons/twitter.svg'
+import instagram from '../icons/instagram.svg'
+import gmail from '../icons/gmail.svg'
+import github from '../icons/github.svg'
 
 class Contact extends Component {
   // Just change the page number depending on route?
@@ -15,7 +19,49 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <div className="contact__content" dangerouslySetInnerHTML={{ __html: this.props.page.rendered }} />
+        <div
+          className="contact__content"
+          dangerouslySetInnerHTML={{ __html: this.props.page.rendered }}
+        />
+        <ul className="social">
+          <li>
+            <a
+              href="https://github.com/berraknil/portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/berraknil/portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gmail} alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/berraknil/portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagram} alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/berraknil/portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="" />
+            </a>
+          </li>
+
+        </ul>
       </div>
     )
   }
