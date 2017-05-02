7551d891a6291bfddfba163615cd754bd9bb7227
Berrak Nil <berraknil@gmail.com>
2017-05-02 12:46:48 +0300

    arrow funcs in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_CONTACT_PAGE = 'FETCH_CONTACT_PAGE'
 
 const URL = 'http://developtests.com/wp-json/wp/v2'
 
-export function fetchPosts() {
+export const fetchPosts = () => {
   const request = axios.get(`${URL}/posts`)
   return {
     type: FETCH_POSTS,
@@ -14,7 +14,7 @@ export function fetchPosts() {
   }
 }
 
-export function fetchAboutPage() {
+export const fetchAboutPage = () => {
   const request = axios.get(`${URL}/pages/2`)
   return {
     type: FETCH_ABOUT_PAGE,
@@ -22,7 +22,7 @@ export function fetchAboutPage() {
   }
 }
 
-export function fetchContactPage() {
+export const fetchContactPage = () => {
   const request = axios.get(`${URL}/pages/19`)
   return {
     type: FETCH_CONTACT_PAGE,
